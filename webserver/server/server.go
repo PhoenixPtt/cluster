9cf398011a7a15601f008ee6682bbffa49666ec2
johnson <johnson_magic@126.com>
2020-08-17 10:50:10 +0800

    0817添加或修改webserver适配优雅关闭功能的代码

--- webserver/server/server.go
+++ webserver/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
 	"os"
@@ -10,10 +11,11 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/gin-gonic/gin"
-
 	"webserver/router"
 )
+
+// 定义全局server对象
+var srv *http.Server
 
 // 供外部调用的初始化web服务端的方法
 func Init() error {
@@ -27,7 +29,7 @@ func Init() error {
 		return err
 	} else { // 正常初始化，则执行可以实现优雅关闭web服务器的操作
 		// 初始化一个web server
-		srv := &http.Server{
+		srv = &http.Server{
 			Addr:    ":8000",
 			Handler: serEngine,
 		}
@@ -35,30 +37,39 @@ func Init() error {
 		// Initializing the server in a goroutine so that
 		// it won't block the graceful shutdown handling below
 		go func() {
-			log.Printf("cluster web server is listening %v\n", srv.Addr)
+			log.Println("cluster web server is listening %v\n", srv.Addr)
 			if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-				log.Fatalf("cluster web server is listen error: %s\n", err)
+				log.Println("cluster web server is listen error: %s\n", err)
 			}
 		}()
 
-		// Wait for interrupt signal to gracefully shutdown the server with a timeout of 5 seconds.
-		quit := make(chan os.Signal)
-		// kill (no param) default send syscall.SIGTERM
-		// kill -2 is syscall.SIGINT
-		// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
-		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-		<-quit
-		log.Println("Shutting down cluster web server...")
-
-		// The context is used to inform the server it has 5 seconds to finish the request it is currently handling
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		if err := srv.Shutdown(ctx); err != nil {
-			log.Fatal("cluster web server forced to shutdown:", err)
-		}
-
-		log.Println("cluster web server exiting")
-
 		return nil
 	}
+}
+
+// 等待系统的中断信号
+func WaitForInterruptSignal() {
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -1 is syscall.SIGHUP, Hangup detected on controlling terminal or death of controlling process, 例如关闭控制终端
+	// kill -2 is syscall.SIGINT, 这是一个 Interrupt from keyboard 的退出信号，例如Ctrl+c
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	<-quit
+}
+
+// webserver关闭功能函数
+func Stop() {
+	log.Println("Shutting down cluster web server...")
+
+	// The context is used to inform the server it has 5 seconds to finish the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Println("cluster web server forced to shutdown:", err)
+		return
+	}
+
+	log.Println("cluster web server exiting")
 }
--- webservermain/main.go
+++ webservermain/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"clusterServer/clusterServer"
 	"fmt"
 	"time"
 	"webserver/server"
 	"webservermain/SysResMonitor"
-
-	"clusterServer/clusterServer"
 )
 
 func main() {
 	// 输出集群服务端启动信息，并启动集群服务端
-	fmt.Println("集群服务端 准备启动...")
+	fmt.Println("集群服务端 启动")
 	go clusterServer.Start()
-	fmt.Println("集群服务端 启动中...")
+
+	// 初始化集群的web服务端
+	fmt.Println("集群web服务端 启动")
+	if err := server.Init(); err != nil {
+		fmt.Println("cluster web server init failure, error is : ", err)
+		return
+	}
 
 	// 监测主进程的CPU内存使用率
 	go func() {
@@ -31,8 +36,14 @@ func main() {
 		}
 	}()
 
-	// 初始化集群的web服务端
-	if err := server.Init(); err != nil {
-		fmt.Println("cluster web server init failure, error is : ", err)
-	}
+	// 等待系统结束命令
+	server.WaitForInterruptSignal()
+
+	// 停止集群的web服务端
+	fmt.Println("集群web服务端 正在停止...")
+	server.Stop()
+
+	// 停止集群服务端
+	fmt.Println("集群服务端 正在停止...")
+
 }
