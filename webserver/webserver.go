22d6dda437ee314025acc337a8fab5e0141ebcfa
johnson <johnson_magic@126.com>
2020-08-17 11:36:06 +0800

    0817在webserver.go中适配优雅关闭的功能

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -6,8 +6,20 @@ import (
 )
 
 func main() {
-	// 初始化集群的web服务器
+	// 初始化集群的web服务端
+	fmt.Println("集群web服务端 启动")
 	if err := server.Init(); err != nil {
 		fmt.Println("cluster web server init failure, error is : ", err)
+		return
 	}
+
+	// 等待系统结束命令
+	server.WaitForInterruptSignal()
+
+	// 停止集群的web服务端
+	fmt.Println("集群web服务端 正在停止...")
+	server.Stop()
+
+
+	fmt.Println("集群web服务端 调试程序已关闭")
 }
