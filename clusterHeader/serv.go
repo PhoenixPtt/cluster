adbddbc17748ea433dcc26e9ac0d9c7ee4e7aff0
LJH <LJH@LJH>
2021-01-18 16:45:16 +0800

    ljh

--- clusterHeader/serv.go
+++ clusterHeader/serv.go
@@ -10,12 +10,6 @@ const (
 	FLAG_SERV       = "SVCS"
 	FLAG_SERV_CTRL  = "SVCS_CTRL" // 启动服务 停止服务 删除服务 扩缩容 重启服务
  	FLAG_SERV_LIST  = "SVCS_LIST"   // 获取服务列表
-	FLAG_SERV_CREATE  = "SVCS_CRET" // 创建服务
-	FLAG_SERV_START   = "SVCS_START" // 启动服务
-	FLAG_SERV_STOP    = "SVCS_STOP" // 停止服务
-	FLAG_SERV_REMOVE  = "SVCS_REMV" // 删除服务
-	FLAG_SERV_SCALE   = "SVCS_SCAL" // 扩缩容
-	FLAG_SERV_RESTART = "SVCS_REST" // 重启服务
 	FLAG_SERV_STATS = "SVCS_STAT"
 	FLAG_SERV_INFO  = "SVCS_INFO"
 )
@@ -24,12 +18,6 @@ const (
 const (
 	FLAG_CTNS   	 = "CTNS"
 	FLAG_CTNS_CTRL   = "CTNS_CTRL"  // 启动容器 停止容器 强制停止容器 删除容器 获取容器日志 获取容器详细信息
-	FLAG_CTNS_CRET   = "CTNS_CRET"  // 创建容器
-	FLAG_CTNS_START   = "CTNS_START" // 启动容器
-	FLAG_CTNS_STOP   = "CTNS_STOP"  // 停止容器
-	FLAG_CTNS_FCST   = "CTNS_FCST"  // 强制停止容器
-	FLAG_CTNS_REMV   = "CTNS_REMV"  // 删除容器
-	FLAG_CTNS_LOG    = "CTNS_LOG"   // 获取容器日志
 	FLAG_CTNS_STATS  = "CTNS_STAT" // 获取容器状态
 	//FLAG_CTNS_INFO   = "CTNS_INFO"  // 获取容器详细信息
 )
--- clusterServer/clusterServer/procFlagSERV.go
+++ clusterServer/clusterServer/procFlagSERV.go
@@ -33,89 +33,19 @@ func procFlagSERV(token interface{}, data interface{}, respChan chan<- interface
 		for  i:=uint32(0); i<r.Count; i++ {
 			r.Service[i].Id = sNames[i];
 		}
-	case header.FLAG_SERV_CREATE: // 创建服务
-		if len(r.Oper.Par)>0  {
-			g_controller.CreateSvcFromFile(r.Oper.Par[0].Value, controller.YML_FILE)
-		} else {
-			r.Oper.Success = false		// 默认操作成功
-			r.Oper.Err = "服务配置文件错误！无法正确创建服务。"
-		}
-	case header.FLAG_SERV_CTRL:// 启动服务 停止服务 删除服务 扩缩容 重启服务
-		if len(r.Oper.Par)>0  {
-			svrName := r.Oper.Par[0].Value;
-			if g_controller.Contains(svrName)  {
-				service := g_controller.GetSvc(svrName)
-				//fmt.Printf("您选择的服务：%#v\n", service)
-				switch r.Oper.Par[0].Name {
-				case header.FLAG_SERV_START:
-					g_controller.StartSvc(service.SvcName)
-				case header.FLAG_SERV_STOP:
-					g_controller.StopSvc(service.SvcName)
-				case header.FLAG_SERV_REMOVE:
-					g_controller.RemoveSvc(service.SvcName)
-				case header.FLAG_SERV_SCALE:
-					g_controller.ScaleSvc(service.SvcName, 8)
-				case header.FLAG_SERV_RESTART:
-					g_controller.StopSvc(service.SvcName)
-					g_controller.StartSvc(service.SvcName)
-				}
-			} else {
-				r.Oper.Success = false		// 默认操作成功
-				r.Oper.Err = "服务[" + svrName + "]不存在"
-			}
-		} else {
-			r.Oper.Success = false		// 默认操作成功
-			r.Oper.Err = "请指定服务名称。"
-		}
-	case header.FLAG_SERV_STATS:
+	case header.FLAG_SERV_STATS: // 服务转台
+		r = *(g_controller.WaitWebService())
+		r.Oper.Success = true		// 默认操作成功
+		r.Oper.Progress = 100		// 默认操作完成
+		r.Oper.Err = ""				// 默认无错误
+	case header.FLAG_SERV_CTRL: // 服务转台
+		pSvcOperaTruck :=  controller.WebService2ServiceOperTruck(&r)
+		g_controller.PutService(pSvcOperaTruck)
 
-	case header.FLAG_SERV_INFO :
-
-	case header.FLAG_CTNS_CRET:  // 创建容器
-			if len(r.Oper.Par) == 0 {
-				r.Oper.Success = false // 默认操作成功
-				r.Oper.Err = "请指定镜像名称。"
-			} else {
-				imageName := r.Oper.Par[0].Value
-				if len(r.Oper.Par) == 1 {
-					hs := g_controller.GetNodeList(controller.ALL_NODES)
-					for _,h := range(hs) {
-						go createContainer(imageName, h)
-					}
-				} else {
-					for i := 1; i < len(r.Oper.Par); i++ {
-						go createContainer(imageName, r.Oper.Par[i].Value)
-					}
-				}
-			}
-
-	case header.FLAG_CTNS_CTRL: // 启动容器
-		if len(r.Oper.Par) >= 1 {
-			switch r.Oper.Par[0].Name {
-			case header.FLAG_CTNS_START:
-				go ctrlContainer(r.Oper.Par[0].Value, ctn.START)
-			case header.FLAG_CTNS_STOP:
-				go ctrlContainer(r.Oper.Par[0].Value, ctn.STOP)
-			case header.FLAG_CTNS_FCST:  // 强制停止容器
-				go ctrlContainer(r.Oper.Par[0].Value, ctn.KILL)
-			case header.FLAG_CTNS_REMV:  // 删除容器
-				go ctrlContainer(r.Oper.Par[0].Value, ctn.REMOVE)
-			case header.FLAG_CTNS_LOG :   // 获取容器日志
-				go ctrlContainer(r.Oper.Par[0].Value, ctn.GETLOG)
-			case header.FLAG_CTNS_STATS : // 获取容器状态
-				go ctrlContainer(r.Oper.Par[0].Value, ctn.INSPECT)
-			default:
-				r.Oper.Success = false					// 操作失败
-				r.Oper.Err = "控制容器参数错误！"				// 操作失败信息
-			}
-		} else {
-			r.Oper.Success = false					// 操作失败
-			r.Oper.Err = "控制容器至少需要容器ID和控制类型！"				// 操作失败信息
-		}
-
-	default:						// 其它操作处理
-		r.Oper.Success = false					// 操作失败
-		r.Oper.Err = "子标识错误！"				// 操作失败信息
+	//
+	//default:						// 其它操作处理
+	//	r.Oper.Success = false					// 操作失败
+	//	r.Oper.Err = "子标识错误！"				// 操作失败信息
 	}
 
 	// 写入数据到通道
